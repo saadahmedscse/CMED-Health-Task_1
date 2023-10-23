d5e01325917bc35a5c3fbcc8543f9ea6db96deb3
saadahmedscse <saadahmedscse@gmail.com>
2023-10-23 18:55:36 +0600

    checked and requested storage write permission

--- app/src/main/java/com/saadahmedev/cmeddownloader/MainActivity.kt
+++ app/src/main/java/com/saadahmedev/cmeddownloader/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.saadahmedev.cmeddownloader
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.saadahmedev.cmeddownloader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        const val STORAGE_WRITE_REQUEST_CODE = 99
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDownload.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                //start download
+            }
+            else {
+                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_WRITE_REQUEST_CODE)
+                } else {
+                    //start download
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == STORAGE_WRITE_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //start download
+        } else {
+            Toast.makeText(this, "Storage write permission is required", Toast.LENGTH_SHORT).show()
+        }
     }
 }
